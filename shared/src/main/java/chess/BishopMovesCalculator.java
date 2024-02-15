e64af96b14d391350fa4c35b4174bc43325b63b7
CarterG2099 <gividenc@byu.edu>
2024-02-15 14:56:27 -0700

    reformatted code

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -3,16 +3,16 @@ package chess;
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class BishopMovesCalculator implements PieceMovesCalculator{
+public class BishopMovesCalculator implements PieceMovesCalculator {
     private final Collection<ChessMove> chessMoveCollection = new ArrayList<ChessMove>();
 
     @Override
-    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position, ChessGame.TeamColor color){
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position, ChessGame.TeamColor color) {
         int[][] directions = {{1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
-        for (int[] move : directions){
+        for (int[] move : directions) {
             int row = position.getRow();
             int col = position.getColumn();
-            while(true) {
+            while (true) {
                 row += move[0];
                 col += move[1];
                 //Check for out of bounds
@@ -20,13 +20,12 @@ public class BishopMovesCalculator implements PieceMovesCalculator{
                 ChessPosition newPosition = new ChessPosition(row, col);
                 ChessPiece pieceAtPosition = board.getPiece(newPosition);
                 //Empty space
-                if (pieceAtPosition == null) chessMoveCollection.add(new ChessMove(position, newPosition,null));
-                //Opponent occupies the space - capture it
+                if (pieceAtPosition == null) chessMoveCollection.add(new ChessMove(position, newPosition, null));
+                    //Opponent occupies the space - capture it
                 else if (pieceAtPosition.getTeamColor() != color) {
-                    chessMoveCollection.add(new ChessMove(position, newPosition,null));
+                    chessMoveCollection.add(new ChessMove(position, newPosition, null));
                     break;
-                }
-                else break; // Same color
+                } else break; // Same color
             }
         }
         return chessMoveCollection;
