b18b70179c714cb9eeb6da0dd273dbd618e24dd6
CarterG2099 <gividenc@byu.edu>
2024-03-14 13:59:04 -0600

    Reformat

--- client/src/main/java/ui/ChessBoardUI.java
+++ client/src/main/java/ui/ChessBoardUI.java
@@ -21,10 +21,12 @@ public class ChessBoardUI {
     private static final String O = " O ";
     private static final Random rand = new Random();
     private static final ChessBoard board = new ChessBoard();
+
     public enum Orientation {
         WHITE,
         BLACK
     }
+
     public static void main(String[] args) {
         board.resetBoard();
         drawChessBoard(board, Orientation.WHITE);
@@ -46,22 +48,22 @@ public class ChessBoardUI {
         drawChessBoard(board, Orientation.BLACK);
     }
 
-    public static void drawChessBoardSpacer(PrintStream out){
+    public static void drawChessBoardSpacer(PrintStream out) {
         out.print(SET_BG_COLOR_BLACK);
         out.print(NEW_LINE);
     }
 
-    private static void drawHeaders(PrintStream out, Orientation orientation){
+    private static void drawHeaders(PrintStream out, Orientation orientation) {
         out.print(SET_BG_COLOR_DARK_GREY);
 
-        String[] headers = {" a "," b "," c "," d "," e "," f "," g "," h "};
+        String[] headers = {" a ", " b ", " c ", " d ", " e ", " f ", " g ", " h "};
         drawEmptyHeader(out);
-        if (orientation == Orientation.WHITE){
-            for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol){
+        if (orientation == Orientation.WHITE) {
+            for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
                 drawHeader(out, headers[boardCol]);
-            }        }
-        else {
-            for (int boardCol = BOARD_SIZE_IN_SQUARES - 1; boardCol >= 0; --boardCol){
+            }
+        } else {
+            for (int boardCol = BOARD_SIZE_IN_SQUARES - 1; boardCol >= 0; --boardCol) {
                 drawHeader(out, headers[boardCol]);
             }
         }
@@ -71,38 +73,37 @@ public class ChessBoardUI {
         out.print(NEW_LINE);
     }
 
-    private static void drawHeader(PrintStream out, String headerText){
+    private static void drawHeader(PrintStream out, String headerText) {
         out.print(SET_TEXT_BOLD);
         out.print(SET_TEXT_COLOR_MAGENTA);
         out.print(headerText);
     }
 
-    private static void drawEmptyHeader(PrintStream out){
+    private static void drawEmptyHeader(PrintStream out) {
         out.print("   ");
     }
 
-    private static void drawBoardWithColumns(PrintStream out, ChessBoard board, Orientation orientation){
-        String[] columns = {" 1 "," 2 "," 3 "," 4 "," 5 "," 6 "," 7 "," 8 "};
-        if (orientation == Orientation.WHITE){
-            for (int boardRow = BOARD_SIZE_IN_SQUARES; boardRow > 0; boardRow--){
+    private static void drawBoardWithColumns(PrintStream out, ChessBoard board, Orientation orientation) {
+        String[] columns = {" 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 "};
+        if (orientation == Orientation.WHITE) {
+            for (int boardRow = BOARD_SIZE_IN_SQUARES; boardRow > 0; boardRow--) {
                 out.print(SET_BG_COLOR_LIGHT_GREY);
-                drawColumn(out, columns[boardRow-1]);
+                drawColumn(out, columns[boardRow - 1]);
                 drawBoardRow(out, boardRow, board, orientation);
-                drawColumn(out, columns[boardRow-1]);
+                drawColumn(out, columns[boardRow - 1]);
                 out.print(SET_BG_COLOR_BLACK);
                 out.print(NEW_LINE);
             }
 
-        }
-        else {
-            for (int boardRow = 1; boardRow <= BOARD_SIZE_IN_SQUARES; boardRow++){
-            out.print(SET_BG_COLOR_LIGHT_GREY);
-            drawColumn(out, columns[boardRow-1]);
-            drawBoardRow(out, boardRow, board, orientation);
-            drawColumn(out, columns[boardRow-1]);
-            out.print(SET_BG_COLOR_BLACK);
-            out.print(NEW_LINE);
-           }
+        } else {
+            for (int boardRow = 1; boardRow <= BOARD_SIZE_IN_SQUARES; boardRow++) {
+                out.print(SET_BG_COLOR_LIGHT_GREY);
+                drawColumn(out, columns[boardRow - 1]);
+                drawBoardRow(out, boardRow, board, orientation);
+                drawColumn(out, columns[boardRow - 1]);
+                out.print(SET_BG_COLOR_BLACK);
+                out.print(NEW_LINE);
+            }
         }
     }
 
@@ -113,17 +114,16 @@ public class ChessBoardUI {
         out.print(colText);
     }
 
-    private static void drawBoardRow(PrintStream out, int boardRow, ChessBoard board, Orientation orientation){
-        if (orientation == Orientation.WHITE){
-            for (int boardCol = 1; boardCol <= BOARD_SIZE_IN_SQUARES; boardCol++){
+    private static void drawBoardRow(PrintStream out, int boardRow, ChessBoard board, Orientation orientation) {
+        if (orientation == Orientation.WHITE) {
+            for (int boardCol = 1; boardCol <= BOARD_SIZE_IN_SQUARES; boardCol++) {
                 int tileToInt = getTileColor(boardRow, boardCol);
                 ChessPosition position = new ChessPosition(boardRow, boardCol);
                 ChessPiece piece = board.getPiece(position);
                 drawTile(out, piece, tileToInt);
             }
-        }
-        else {
-            for (int boardCol = BOARD_SIZE_IN_SQUARES; boardCol > 0; boardCol--){
+        } else {
+            for (int boardCol = BOARD_SIZE_IN_SQUARES; boardCol > 0; boardCol--) {
                 int tileToInt = getTileColor(boardRow, boardCol);
                 ChessPosition position = new ChessPosition(boardRow, boardCol);
                 ChessPiece piece = board.getPiece(position);
@@ -132,26 +132,24 @@ public class ChessBoardUI {
         }
     }
 
-    private static int getTileColor(int boardRow, int boardCol){
+    private static int getTileColor(int boardRow, int boardCol) {
         return (boardRow + boardCol) % 2;
     }
 
-    private static void drawTile(PrintStream out, ChessPiece piece, int tileToInt){
-        if (tileToInt == 0){
+    private static void drawTile(PrintStream out, ChessPiece piece, int tileToInt) {
+        if (tileToInt == 0) {
             out.print(SET_BG_COLOR_LIGHT_GREY);
-        }
-        else {
+        } else {
             out.print(SET_BG_COLOR_BLUE);
         }
-        if (piece == null){
+        if (piece == null) {
             out.print("   ");
-        }
-        else {
+        } else {
             drawChessPiece(out, piece);
         }
     }
 
-    private static void drawChessPiece(PrintStream out, ChessPiece piece){
+    private static void drawChessPiece(PrintStream out, ChessPiece piece) {
         if (piece.getTeamColor() == ChessGame.TeamColor.WHITE) {
             out.print(SET_TEXT_COLOR_WHITE);
             switch (piece.getPieceType()) {
@@ -160,9 +158,9 @@ public class ChessBoardUI {
                 case BISHOP -> out.print(" B ");
                 case ROOK -> out.print(" R ");
                 case QUEEN -> out.print(" Q ");
-                case KING -> out.print(" K ");}
-        }
-        else {
+                case KING -> out.print(" K ");
+            }
+        } else {
             out.print(SET_TEXT_COLOR_BLACK);
             switch (piece.getPieceType()) {
                 case PAWN -> out.print(" P ");
@@ -175,14 +173,10 @@ public class ChessBoardUI {
         }
     }
 
-    private static void setBlack(PrintStream out){
+    private static void setBlack(PrintStream out) {
         out.print(SET_BG_COLOR_BLACK);
         out.print(SET_TEXT_COLOR_BLACK);
     }
 
 
 }
-
-//How to get user input
-// Scanner scanner = new Scanner(System.in);
-// String line = scanner.nextLine();
